6d79a90de7f9c1b7633309f241842cec6a4d73e4
Demian Kalinchuk <demonation20@gmail.com>
2021-11-10 17:22:02 +0200

    changes class

---
+++ src/main/java/selenium_testng.java
@@ -0,0 +1,14 @@
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.*;
+import org.testng.annotations.Test;
+
+public class selenium_testng {
+
+
+    @Test
+    public void ChromeTest()
+    {
+     System.out.println("hello test #1");
+    }
+}
